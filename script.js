d799df202b37cd67c4f9d931ece6aea8c496d7e4
akshvnd2007-star <akshvnd.2007@gmail.com>
2026-03-22 15:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let index=0;
-const slides=document.getElementById("slides");
-const totalSlides = slides.children.length;
-function showSlide(){
-slides.style.transform = `translateX(-${index * 500}px)`;
-}
-function nextSlide(){
-index = (index + 1) % totalSlides;
-showSlide();
-}
-function prevSlide(){
-index = (index - 1 + totalSlides) % totalSlides;
-showSlide();
+window.onload = function() {
+  let index = 0;
+  const slides = document.getElementById("slides");
+  const totalSlides = slides.children.length;
+
+  function showSlide(){
+    slides.style.transform = `translateX(-${index * 500}px)`;
+  }
+
+  window.nextSlide = function(){
+    index = (index + 1) % totalSlides;
+    showSlide();
+  }
+
+  window.prevSlide = function(){
+    index = (index - 1 + totalSlides) % totalSlides;
+    showSlide();
+  }
+};
