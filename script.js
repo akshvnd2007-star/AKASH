d4c97a903e3f4d40531dd5655f6f7f5cb658564d
akshvnd2007-star <akshvnd.2007@gmail.com>
2026-03-22 15:33:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 let index=0;
-
 const slides=document.getElementById("slides");
-
 const totalSlides = slide.children.length;
-
 function showSlide(){
-
 slides.style.transform = `translateX(-${index * 500}px)`;
-
 }
 function nextSlide(){
-
 index = (index + 1) % totalSlides;
-
 showSlide();
-
 }
-
 function prevSlide(){
-
 index = (index - 1 + totalSlides) % totalSlides;
-
 showSlide();
